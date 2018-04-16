e9a9d12fdf8cd6c37b9aeb1ddd2e3693fbcea5b6
Oli Bridgman <oli.bridgman@gmail.com>
2018-04-16 17:02:24 +1200

    Add basic routes for events

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // The port the express app will listen on
 const port: number = Number(process.env.PORT) || 3000;
 
+const router: express.Router = express.Router();
+
+router.get("/", async function(req, res) {
+  const results = await db.events.all();
+  res.send(results);
+});
+
+router.post("/", async function(req, res) {
+  const { cid, type, payload } = req.body;
+  const event = { cid, type, payload };
+
+  const result = db.events.add(event);
+  res.send(result);
+});
+
+app.use("/", router);
+
 // Serve the application at the given port
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
