bd701220c98441761bbf8c4ac1e1fcb2949b8e70
Oli Bridgman <oli.bridgman@gmail.com>
2018-04-16 16:57:19 +1200

    Use body parser in app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 require("dotenv-safe").config({ allowEmptyValues: true });
 
 import express from "express";
+import bodyParser from "body-parser";
+
 import { db } from "./db";
 
 // Create a new express application instance
 const app: express.Application = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // The port the express app will listen on
 const port: number = Number(process.env.PORT) || 3000;
